694ba916e39b4adaabe5b3488a51320e68e502eb
Wataru MIYAGUNI <gonngo@gmail.com>
2014-06-28 12:30:49 +0900

    Add golang libraries

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,21 +1,33 @@
+define goget ($package = $title) {
+  exec { "go get -u ${package}":
+  }
+}
+
 node default {
   # This is required to apply some manifests since these are not using
   # appropriate user, group or full path to the command.
   Exec {
-    group => "staff",
+    group => 'staff',
     user => $boxen_user,
     path => [
-      "/usr/local/bin",
-      "/usr/bin",
-      "/bin",
-      "/usr/sbin",
-      "sbin"
-    ]
+      "/Users/${boxen_user}/.homebrew/bin",
+      '/usr/local/bin',
+      '/usr/bin',
+      '/bin',
+      '/usr/sbin',
+      '/sbin'
+    ],
+    environment => ["GOPATH=/Users/${boxen_user}/.go"]
+  }
+
+  Package {
+    provider => homebrew,
+    require => Class['homebrew']
   }
 
   # This is also required to apply some manifests correctly.
   File {
-    group => "staff",
+    group => 'staff',
     owner => $boxen_user
   }
 
@@ -25,4 +37,14 @@ node default {
   }
 
   # Your manifests goes here.
+
+  include osx::dock::autohide
+
+  $go_packages = [
+    'code.google.com/p/go.tools/cmd/goimports',
+    'github.com/golang/lint/golint',
+    'github.com/nsf/gocode'
+  ]
+
+  goget { $go_packages: }
 }
